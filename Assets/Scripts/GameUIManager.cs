903370a9ca56654d8bfb41842dda400c1c5e2722
denizalbayrak <deniz.al26@gmail.com>
2024-11-08 22:32:06 +0300

    UI update

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -11,8 +10,10 @@ public class GameUIManager : MonoBehaviour
     public GameObject recipeTopUIPrefab;
     public GameObject recipeToggleUIPrefab;
     public GameObject MoreRecipeUI;
-    public Transform ownedRecipesContainer; // Horizontal layout olan container
-    public Transform chooseRecipeContainer; // Tüm tariflerin listelendiði panel
+    public Transform ownedRecipesContainer; // Horizontal layout container
+    public Transform chooseRecipeContainer; // Panel listing all recipes
+    public GameObject WarningPanel; // Reference to the warning panel
+    public TextMeshProUGUI WarningPanelText; // Reference to the warning panel
 
     private void Awake()
     {
@@ -25,67 +26,64 @@ public class GameUIManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     public void LoadLevelRecipesUI()
     {
-        // Kullanýcýnýn tariflerini SaveData'dan al
-        List<string> ownedRecipes = GameManager.Instance.currentSaveData.ownedRecipeNames;
-        // Mevcut tarif UI'lerini temizle
-        foreach (Transform child in ownedRecipesContainer)
-        {
-            Destroy(child.gameObject);
-        }
-        int visibleRecipeCount = 0;
-        foreach (var recipeName in ownedRecipes)
-        {
-            Recipe recipe = ItemHolder.Instance.GetRecipe(recipeName);
-            if (recipe != null)
-            {
-                if (visibleRecipeCount < 3)
-                {
-                    GameObject newRecipeUI = Instantiate(recipeTopUIPrefab, ownedRecipesContainer);
-                newRecipeUI.GetComponentInChildren<TextMeshProUGUI>().text = recipe.recipeName;
-                newRecipeUI.transform.GetChild(0).GetComponent<Image>().sprite = recipe.recipeImage;
-                }
-                visibleRecipeCount++;
-            }
-            else
-            {
-                Debug.LogWarning("Recipe not found in ItemHolder: " + recipeName);
-            }
-        }
         LoadChooseRecipesUI();
-
     }
+
     public void LoadChooseRecipesUI()
     {
         List<string> ownedRecipes = GameManager.Instance.currentSaveData.ownedRecipeNames;
 
-        // Mevcut tarif UI'lerini temizle
+        // Clear existing recipe UI elements
         foreach (Transform child in chooseRecipeContainer)
         {
             Destroy(child.gameObject);
         }
+
+        // Check if any recipes have been selected previously
+        bool hasSelectedRecipes = GameManager.Instance.currentSaveData.selectedRecipeNames != null &&
+                                  GameManager.Instance.currentSaveData.selectedRecipeNames.Count > 0;
+
         foreach (var recipeName in ownedRecipes)
         {
             Recipe recipe = ItemHolder.Instance.GetRecipe(recipeName);
             if (recipe != null)
             {
-                // `chooseRecipeContainer` için toggle'ý oluþtur
                 GameObject newRecipeToggleUI = Instantiate(recipeToggleUIPrefab, chooseRecipeContainer);
                 newRecipeToggleUI.GetComponentInChildren<TextMeshProUGUI>().text = recipe.recipeName;
                 newRecipeToggleUI.transform.GetChild(0).GetComponent<Image>().sprite = recipe.recipeImage;
 
-                // Toggle bileþenini al ve ayarla
                 Toggle toggle = newRecipeToggleUI.GetComponentInChildren<Toggle>();
                 if (toggle != null)
                 {
                     toggle.onValueChanged.AddListener(isOn => OnToggleValueChanged(isOn, recipe));
 
-                    // Toggle'ýn baþlangýç durumunu ayarla
-                    bool isSelected = recipe.recipeName == "Beer" || GameManager.Instance.currentSaveData.selectedRecipeNames.Contains(recipe.recipeName);
+                    bool isSelected;
+
+                    if (!hasSelectedRecipes)
+                    {
+                        // If no selections have been made, default to "Beer" only
+                        isSelected = recipe.recipeName == "Beer";
+                        if (isSelected)
+                        {
+                            // Add "Beer" to selectedRecipeNames if not already added
+                            if (!GameManager.Instance.currentSaveData.selectedRecipeNames.Contains(recipe.recipeName))
+                            {
+                                GameManager.Instance.currentSaveData.selectedRecipeNames.Add(recipe.recipeName);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Use previously saved selections
+                        isSelected = GameManager.Instance.currentSaveData.selectedRecipeNames.Contains(recipe.recipeName);
+                    }
+
                     toggle.isOn = isSelected;
 
-                    // Yalnýzca `Toggle` seçiliyse `ownedRecipesContainer`'a ekle
+                    // Add to ownedRecipesContainer if selected
                     if (isSelected && !IsRecipeInOwnedContainer(recipe.recipeName))
                     {
                         AddRecipeToOwnedContainer(recipe);
@@ -97,17 +95,30 @@ public class GameUIManager : MonoBehaviour
                 Debug.LogWarning("Recipe not found in ItemHolder: " + recipeName);
             }
         }
+
+        // After setting up toggles, check if no recipes are selected
+        if (GameManager.Instance.currentSaveData.selectedRecipeNames.Count == 0)
+        {
+            // Show warning panel
+            WarningPanel.SetActive(true);
+            WarningPanelText.text = "You must choose at least one recipe.";
+        }
+        else
+        {
+            // Hide warning panel
+            WarningPanel.SetActive(false);
+        }
     }
 
     private void AddRecipeToOwnedContainer(Recipe recipe)
     {
-        // Zaten mevcutsa ekleme
+        // Do not add if already present
         if (IsRecipeInOwnedContainer(recipe.recipeName))
         {
             return;
         }
 
-        // Yeni bir UI oluþtur ve ownedRecipesContainer'a ekle
+        // Create a new UI element and add it to ownedRecipesContainer
         GameObject newRecipeUI = Instantiate(recipeTopUIPrefab, ownedRecipesContainer);
         newRecipeUI.GetComponentInChildren<TextMeshProUGUI>().text = recipe.recipeName;
         newRecipeUI.transform.GetChild(0).GetComponent<Image>().sprite = recipe.recipeImage;
@@ -126,27 +137,13 @@ public class GameUIManager : MonoBehaviour
         return false;
     }
 
-    private bool IsRecipeInChooseContainer(string recipeName)
-    {
-        foreach (Transform child in chooseRecipeContainer)
-        {
-            TextMeshProUGUI textComponent = child.GetComponentInChildren<TextMeshProUGUI>();
-            if (textComponent != null && textComponent.text == recipeName)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    
     private void OnToggleValueChanged(bool isOn, Recipe recipe)
     {
         if (isOn)
         {
-            // Tarif seçildi, ownedRecipesContainer'a ekle
+            // Recipe selected, add to ownedRecipesContainer
             AddRecipeToOwnedContainer(recipe);
-            // Seçili tarifleri SaveData'ya ekle
+            // Add to selectedRecipeNames in SaveData
             if (!GameManager.Instance.currentSaveData.selectedRecipeNames.Contains(recipe.recipeName))
             {
                 GameManager.Instance.currentSaveData.selectedRecipeNames.Add(recipe.recipeName);
@@ -154,20 +151,32 @@ public class GameUIManager : MonoBehaviour
         }
         else
         {
-            // Tarif seçimi kaldýrýldý, ownedRecipesContainer'dan çýkar
+            // Recipe deselected, remove from ownedRecipesContainer
             RemoveRecipeFromOwnedContainer(recipe.recipeName);
-            // Seçili tarifleri SaveData'dan çýkar
+            // Remove from selectedRecipeNames in SaveData
             GameManager.Instance.currentSaveData.selectedRecipeNames.Remove(recipe.recipeName);
         }
 
-        // Deðiþiklikleri kaydet
+        // Save changes
         GameManager.Instance.SaveGame();
+
+        // Check if no recipes are selected
+        if (GameManager.Instance.currentSaveData.selectedRecipeNames.Count == 0)
+        {
+            // Show warning panel
+            WarningPanel.SetActive(true);
+            WarningPanelText.text = "You must choose at least one recipe.";
+        }
+        else
+        {
+            // Hide warning panel
+            WarningPanel.SetActive(false);
+        }
     }
 
-    
     private void RemoveRecipeFromOwnedContainer(string recipeName)
     {
-        // ownedRecipesContainer içindeki tarifleri kontrol et ve verilen isme sahip olaný kaldýr
+        // Check ownedRecipesContainer and remove the recipe with the given name
         foreach (Transform child in ownedRecipesContainer)
         {
             TextMeshProUGUI textComponent = child.GetComponentInChildren<TextMeshProUGUI>();
@@ -178,6 +187,4 @@ public class GameUIManager : MonoBehaviour
             }
         }
     }
-
-   
 }

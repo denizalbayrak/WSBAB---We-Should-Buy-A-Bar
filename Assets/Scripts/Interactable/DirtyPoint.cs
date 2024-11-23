0b41dc09cdf4f39b7314fc072cb041b87f2b4bfa
denizalbayrak <deniz.al26@gmail.com>
2024-11-23 18:00:15 +0300

    Dirty object mechanic

--- Assets/Scripts/Interactable/DirtyPoint.cs
+++ Assets/Scripts/Interactable/DirtyPoint.cs
@@ -5,27 +5,45 @@ public class DirtyPoint : PlacableInteractable
 {
     public int maxCapacity = 2; // DirtyPoint'in maksimum kapasitesi
     public List<GameObject> placedObjects = new List<GameObject>(); // Þu anki bardaklar
-
     public List<Transform> slotTransforms; // Bardaklarýn yerleþeceði slotlar
 
     public void AddPlacedObject(GameObject obj)
     {
+        // Kapasite kontrolü
         if (placedObjects.Count >= maxCapacity)
         {
             Debug.Log("DirtyPoint is full.");
             return;
         }
 
-        placedObjects.Add(obj);
-        obj.transform.SetParent(placementPoint);
+        // Boþ bir slot bul
+        Transform availableSlot = null;
+        foreach (Transform slot in slotTransforms)
+        {
+            if (slot.childCount == 0)
+            {
+                availableSlot = slot;
+                break;
+            }
+        }
 
-        // Bardaklarý slotlara yerleþtir
-        Transform slotTransform = slotTransforms[placedObjects.Count - 1];
-        obj.transform.position = slotTransform.position;
-        obj.transform.rotation = slotTransform.rotation;
+        if (availableSlot == null)
+        {
+            Debug.Log("No available slot to place the object.");
+            return;
+        }
+
+        // Obje yerleþtir
+        placedObjects.Add(obj);
+        obj.transform.SetParent(availableSlot);
+
+        obj.transform.position = availableSlot.position;
+        obj.transform.rotation = availableSlot.rotation;
         obj.transform.localScale = Vector3.one; // Ölçeði ayarlayýn
         obj.GetComponent<BeerGlass>().Dirty();
         obj.SetActive(true);
+
+        Debug.Log("Placed dirty glass on DirtyPoint.");
     }
 
     public override void Interact(GameObject player)
@@ -42,8 +60,12 @@ public class DirtyPoint : PlacableInteractable
                     GameObject objToPickUp = placedObjects[0];
                     placedObjects.RemoveAt(0);
 
+                    // Bardaðý slotundan kaldýr
+                    objToPickUp.transform.SetParent(null);
                     // Bardaðý oyuncuya ver
                     playerInteraction.PickUpObject(objToPickUp);
+
+
                     Debug.Log("Picked up dirty glass from DirtyPoint.");
                 }
                 else
@@ -53,8 +75,20 @@ public class DirtyPoint : PlacableInteractable
             }
             else
             {
-                // Oyuncu bir þey taþýyor, DirtyPoint'e obje yerleþtiremez
-                Debug.Log("Cannot place objects on DirtyPoint.");
+                // Oyuncu bir þey taþýyor, pis bardak yerleþtirmeye çalýþ
+                BeerGlass beerGlass = playerInteraction.CarriedObject.GetComponent<BeerGlass>();
+                if (beerGlass != null && beerGlass.CurrentState == BeerGlass.GlassState.DirtyEmpty)
+                {
+                    // Pis bardaðý DirtyPoint'e yerleþtir
+                    AddPlacedObject(playerInteraction.CarriedObject);
+                    playerInteraction.CarriedObject = null;
+                    playerInteraction.isCarrying = false; // Merkezi yönetim için
+                    playerInteraction.animator.SetBool("isCarry", false);
+                }
+                else
+                {
+                    Debug.Log("Cannot place object. Only dirty glasses can be placed on DirtyPoint.");
+                }
             }
         }
         else
@@ -76,7 +110,19 @@ public class DirtyPoint : PlacableInteractable
             }
             else
             {
-                // Oyuncu bir þey taþýyor, DirtyPoint'e obje yerleþtiremez
+                // Oyuncu bir þey taþýyor, pis bardak yerleþtirebilir mi kontrol et
+                BeerGlass beerGlass = playerInteraction.CarriedObject.GetComponent<BeerGlass>();
+                if (beerGlass != null && beerGlass.CurrentState == BeerGlass.GlassState.DirtyEmpty)
+                {
+                    // Boþ bir slot var mý kontrol et
+                    foreach (Transform slot in slotTransforms)
+                    {
+                        if (slot.childCount == 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
                 return false;
             }
         }
